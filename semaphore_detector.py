87457027968531c0d4126422397f5093c896bcdc
Marko Petrović <156552538+JoksimKaktus@users.noreply.github.com>
2026-04-14 12:36:17 +0200

    Refactor getData function and update area thresholds
    Refactor semaphore detection logic into a function and adjust area thresholds for contour detection.

--- semaphore_detector.py
+++ semaphore_detector.py
@@ -15,13 +15,8 @@ upper_red = np.array([122, 255, 255])
 noblack_low = np.array([1,1,1])
 noblack_upp = np.array([255,255,255])
 
-picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration())
-picam2.start()
-prev_time = 0
 
-
-while True:
+def getData(picam2):
     frame = picam2.capture_array()    # BGR image
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -29,15 +24,12 @@ while True:
 
     mask_green = cv2.inRange(frame_hsv, lower_green, upper_green)
     mask_red = cv2.inRange(frame_hsv, lower_red, upper_red)
-    mask = cv2.bitwise_or(mask_green, mask_red)
 
 
     result_green = cv2.bitwise_and(frame, frame, mask=mask_green)
     result_green = cv2.cvtColor(result_green, cv2.COLOR_BGR2RGB)      # RGB image, only green
-    result_green_hsv = cv2.cvtColor(result_green, cv2.COLOR_BGR2HSV)  # HSV image, only green
     result_red = cv2.bitwise_and(frame, frame, mask=mask_red)
     result_red = cv2.cvtColor(result_red, cv2.COLOR_BGR2RGB)          # RGB image, only red
-    result_red_hsv = cv2.cvtColor(result_red, cv2.COLOR_BGR2HSV)      # HSV image, only red
 
     bw_green = cv2.inRange(result_green, noblack_low, noblack_upp)    # Turning green into white
     bw_red = cv2.inRange(result_red, noblack_low, noblack_upp)        # Turning red into white
@@ -59,24 +51,27 @@ while True:
     for c in contours_g:
         area = cv2.contourArea(c)
         # ignore tiny blobs/noise
-        if area > 750 and area > max_area:
+        if area > 100 and area > max_area:
             x, y, w, h = cv2.boundingRect(c)
-            if h >= w:
+            if h >= w/2:
                 max_area = area
                 max_attr = [x,y,w,h,(0,255,0)]
-                # cv2.rectangle(frame_rgb, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # Find contours and draw rectangles around red object     
     contours_r, _ = cv2.findContours(bw_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for c in contours_r:
         area = cv2.contourArea(c)
         # ignore tiny blobs/noise
-        if area > 750 and area > max_area:
+        if area > 100 and area > max_area:
             x, y, w, h = cv2.boundingRect(c)
-            if h >= w:
+            if h >= w/2:
                 max_area = area
                 max_attr = [x,y,w,h,(0,0,255)]
-                # cv2.rectangle(frame_rgb, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    # 0 - green, 1 - red
+    # center of semaphore
+    # 0 - very far away, 1 - far away, 2 - middle, 3 - close, 4 - very close 
+    ret = [-1,-1,-1] 
 
     if max_area > 0:
         x = max_attr[0]
@@ -84,6 +79,11 @@ while True:
         w = max_attr[2]
         h = max_attr[3]
         color = max_attr[4]
+        if color == (0,255,0):
+            ret[0] = 0
+        else:
+            ret[0] = 1
+
         cv2.rectangle(frame_rgb, (x, y), (x + w, y + h), color, 2)
         # print(x,y,w,h)
         new_x = (x-320)/320
@@ -94,36 +94,26 @@ while True:
         area_left = 0
         area_right = 0
         semaphore_center = (2*new_x + new_w)/2
-        if(semaphore_center > 0):
-            if(semaphore_center < 0.5):
-                print("Semafor je desno")
-            else:
-                print("Semafor je vrlo desno")
-        else:
-            if(semaphore_center > -0.5):
-                print("Semafor je lijevo")
-            else:
-                print("Semafor je vrlo lijevo")
-        
-        if(max_area > 60000):
-            print("Bas blizu")
-        elif(max_area > 30000):
-            print("Blizu")
-        elif(max_area > 10000):
-            print("Srednja razdaljina")
-        elif(max_area > 5000):
-            print("Daleko")
-        else:
-            print("Bas daleko")
+        ret[1] = semaphore_center
+        ret[2] = max_area
 
-        print(max_area/(h * w)*100,'%',"semafor")
-        print("-----------------------------")
+        #if max_area/(h * w)*100 < 0.6:
+         #   return [-1,-1,-1]
+
+    #cv2.imshow("Line", frame_rgb)
+    #cv2.waitKey(1)
+
+    return ret
 
 
-
-    cv2.imshow("Pi Camera", frame_rgb)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cv2.waitKey(1)
 
 cv2.destroyAllWindows()
+
+
+# picam2 = Picamera2()
+# picam2.configure(picam2.create_preview_configuration())
+# picam2.start()
+
+# for i in range(10000):
+#     print(getData(picam2))
